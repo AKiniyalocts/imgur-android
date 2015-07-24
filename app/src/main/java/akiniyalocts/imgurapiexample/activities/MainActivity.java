6591c8b05ae46a17caaa81c710ef55481d9355ae
Christopher Jakob <jakob.chrisli@gmail.com>
2015-07-24 22:43:19 +0200

    Implemented Callback for retrofit call, so we can show a Snackbar when no Internet connection is available

--- app/src/main/java/akiniyalocts/imgurapiexample/activities/MainActivity.java
+++ app/src/main/java/akiniyalocts/imgurapiexample/activities/MainActivity.java
@@ -3,7 +3,7 @@ package akiniyalocts.imgurapiexample.activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
@@ -18,98 +18,116 @@ import akiniyalocts.imgurapiexample.helpers.DocumentHelper;
 import akiniyalocts.imgurapiexample.helpers.IntentHelper;
 import akiniyalocts.imgurapiexample.imgurmodel.ImageResponse;
 import akiniyalocts.imgurapiexample.imgurmodel.Upload;
-import akiniyalocts.imgurapiexample.services.OnImageUploadedListener;
 import akiniyalocts.imgurapiexample.services.UploadService;
-import akiniyalocts.imgurapiexample.utils.aLog;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
-public class MainActivity extends AppCompatActivity implements OnImageUploadedListener{
-  public final static String TAG = MainActivity.class.getSimpleName();
+public class MainActivity extends AppCompatActivity {
+    public final static String TAG = MainActivity.class.getSimpleName();
 
-  /*
-    These annotations are for ButterKnife by Jake Wharton
+    /*
+      These annotations are for ButterKnife by Jake Wharton
+      https://github.com/JakeWharton/butterknife
+     */
+    @Bind(R.id.imageview)
+    ImageView uploadImage;
+    @Bind(R.id.editText_upload_title)
+    EditText uploadTitle;
+    @Bind(R.id.editText_upload_desc)
+    EditText uploadDesc;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
-    https://github.com/JakeWharton/butterknife
+    private Upload upload; // Upload object containging image and meta data
+    private File chosenFile; //chosen file from intent
 
-   */
-  @Bind(R.id.image)ImageView uploadImage;
-  @Bind(R.id.fab) FloatingActionButton uploadBtn;
-  @Bind(R.id.editText_upload_title)EditText uploadTitle;
-  @Bind(R.id.editText_upload_desc)EditText uploadDesc;
-  @Bind(R.id.toolbar) Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-  private Upload upload; // Upload object containging image and meta data
-  private File chosenFile; //chosen file from intent
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
-
-    setSupportActionBar(toolbar);
-  }
-
-  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    Uri returnUri;
-
-    if(requestCode == IntentHelper.FILE_PICK) {
-      if (resultCode == RESULT_OK) {
-        returnUri = data.getData();
-        chosenFile = new File(DocumentHelper.getPath(this, returnUri));
-
-        /*
-          Picasso is a wonderful image loading tool from square inc.
-
-          https://github.com/square/picasso
-         */
-        Picasso.with(this).load(chosenFile).fit().into(uploadImage);
-
-        /*
-          Enable the upload button after the file has been chosen
-         */
-        uploadBtn.setEnabled(true);
-      }
+        setSupportActionBar(toolbar);
     }
 
-  }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri returnUri;
 
-  @OnClick(R.id.image)
-  public void onChooseImage(){
-    uploadDesc.clearFocus();
-    uploadTitle.clearFocus();
-    IntentHelper.chooseFileIntent(this);
-  }
+        if (requestCode != IntentHelper.FILE_PICK) {
+            return;
+        }
 
-  @OnClick(R.id.fab)
-  public void uploadImage(){
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        returnUri = data.getData();
+        String filePath = DocumentHelper.getPath(this, returnUri);
+        //Safety check to prevent null pointer exception
+        if(filePath == null || filePath.isEmpty()) return;
+        chosenFile = new File(filePath);
+
+                /*
+                    Picasso is a wonderful image loading tool from square inc.
+                    https://github.com/square/picasso
+                 */
+        Picasso.with(getBaseContext())
+                .load(chosenFile)
+                .placeholder(R.drawable.ic_photo_library_black)
+                .fit()
+                .into(uploadImage);
+
+    }
+
+
+    @OnClick(R.id.imageview)
+    public void onChooseImage() {
+        uploadDesc.clearFocus();
+        uploadTitle.clearFocus();
+        IntentHelper.chooseFileIntent(this);
+    }
+
+    @OnClick(R.id.fab)
+    public void uploadImage() {
     /*
       Create the @Upload object
      */
-    if(chosenFile == null) return;
-    createUpload(chosenFile);
+        if (chosenFile == null) return;
+        createUpload(chosenFile);
 
     /*
       Start upload
      */
-    new UploadService(upload, this).execute();
-  }
+        new UploadService(this).Execute(upload, new UiCallback());
+    }
 
-  @Override public void onImageUploaded(ImageResponse response) {
-    /*
-      Logging the response from the image upload.
-     */
-    aLog.w(TAG, response.toString());
-  }
+    private void createUpload(File image) {
+        upload = new Upload();
 
-  private void createUpload(File image){
-    upload = new Upload();
+        upload.image = image;
+        upload.title = uploadTitle.getText().toString();
+        upload.description = uploadDesc.getText().toString();
+    }
 
-    upload.image = image;
-    upload.title = uploadTitle.getText().toString();
-    upload.description = uploadDesc.getText().toString();
-  }
+    private class UiCallback implements Callback<ImageResponse> {
+
+        @Override
+        public void success(ImageResponse imageResponse, Response response) {
+
+        }
+
+        @Override
+        public void failure(RetrofitError error) {
+            //Assume we have no connection, since error is null
+            if (error == null) {
+                Snackbar.make(findViewById(R.id.rootView), "No internet connection", Snackbar.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
