b3800d0baf4a0a8e2f11dd0cb44dcadd870114ed
Christopher Jakob <jakob.chrisli@gmail.com>
2015-07-24 22:44:20 +0200

    Indenting + Reformat

--- app/src/main/java/akiniyalocts/imgurapiexample/imgurmodel/ImgurAPI.java
+++ app/src/main/java/akiniyalocts/imgurapiexample/imgurmodel/ImgurAPI.java
@@ -1,5 +1,6 @@
 package akiniyalocts.imgurapiexample.imgurmodel;
 
+import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.Header;
 import retrofit.http.POST;
@@ -8,34 +9,37 @@ import retrofit.mime.TypedFile;
 
 /**
  * Created by AKiniyalocts on 2/23/15.
- *
+ * <p/>
  * This is our imgur API. It generates a rest API via Retrofit from Square inc.
- *
+ * <p/>
  * more here: http://square.github.io/retrofit/
  */
 public interface ImgurAPI {
-  public static String server = "https://api.imgur.com";
+    String server = "https://api.imgur.com";
 
 
-  /****************************************
-   * Upload
-   * Image upload API
-   */
+    /****************************************
+     * Upload
+     * Image upload API
+     */
 
-  /**
-   *
-   * @param auth #Type of authorization for upload
-   * @param title #Title of image
-   * @param description #Description of image
-   * @param albumId #ID for album (if the user is adding this image to an album)
-   * @return
-   */
-  @POST("/3/image")ImageResponse postImage (
-      @Header("Authorization")String auth,
-      @Query("title")String title,
-      @Query("description")String description,
-      @Query("album")String albumId,
-      @Query("account_url")String username,
-      @Body TypedFile file
-  );
+    /**
+     * @param auth        #Type of authorization for upload
+     * @param title       #Title of image
+     * @param description #Description of image
+     * @param albumId     #ID for album (if the user is adding this image to an album)
+     * @param username    username for upload
+     * @param file        image
+     * @param cb          Callback used for success/failures
+     */
+    @POST("/3/image")
+    void postImage(
+            @Header("Authorization") String auth,
+            @Query("title") String title,
+            @Query("description") String description,
+            @Query("album") String albumId,
+            @Query("account_url") String username,
+            @Body TypedFile file,
+            Callback<ImageResponse> cb
+    );
 }
